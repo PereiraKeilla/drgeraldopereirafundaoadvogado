c62ca5972621f2739222c862ede947eb87c06df9
PereiraKeilla <152453024+PereiraKeilla@users.noreply.github.com>
2023-12-09 19:27:15 -0300

    primeiro commit

--- java.js
+++ java.js
@@ -1,4 +1,3 @@
-
 var campo = document.getElementById('whats');
 let dataAtual = new Date();
 console.log(dataAtual.getHours())
@@ -10,4 +9,3 @@ if (dataAtual.getHours() > 18) {
 } else {
     campo.innerHTML = '<a href="https://api.whatsapp.com/send?phone=5527998669847&text=Bom%20dia!%20Obrigado%20pelo%20contato.%20Gostaria%20de%20marcar%20um%20hor%C3%A1rio?"><div><p><img src="img6.jpg" align="left" height="20%" width=15%"></p></div><br></a>'
 }
-
